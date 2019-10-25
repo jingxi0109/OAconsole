ed7abdb0733793e6dbda9afb8b15d746d4210c12
敬熙 陈 <gelly0106@msn.com>
2019-10-25 12:16:43 +0800

    分布，

---
+++ OAconsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OAconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
